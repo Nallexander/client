3423c7f687d106b77fe40f2b74819ca239625e24
Nallexander <alexander.e.lind@gmail.com>
2015-10-29 20:16:54 +0100

    Fix

--- SocketClient.java
+++ SocketClient.java
@@ -16,13 +16,15 @@ class SocketClient extends DISClient {
      * @return The return string from the "socket call"
      */
     public static String runCommand(LabGroup input_lg, String server, int port) {
-	Socket socket = new Socket("joshua.it.uu.se", port);
-	
-	
-	ObjectOutputStream sendToServer = new ObjectOutputStream(socket.getOutputStream());
-	ObjectInputStream receiveFromServer = new ObjectInputStream(socket.getInputStream());
-	sendToServer.writeObject(input_lg);
-	String output = (String) receiveFromServer.readObject();
+	try{
+	    Socket socket = new Socket("server", port);
+	    ObjectOutputStream sendToServer = new ObjectOutputStream(socket.getOutputStream());
+	    ObjectInputStream receiveFromServer = new ObjectInputStream(socket.getInputStream());
+	    sendToServer.writeObject(input_lg);
+	    String output = (String) receiveFromServer.readObject();
+
+	}
+	catch (UnknownHostException e){}
 	/* TODO: Provide the code to create a socket and
 	   connect it to the given port number ("port")
 	   at the specified host ("server"). Then send over
